b06a63b60dc31ae9589ee34f8950eab0bb1026b9
Ivan Tabarino <itabarino@dunelm.com>
2022-05-22 15:38:19 +0100

    feat: add react query with pagination, prefetching and mutations

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery, useMutation } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,13 +24,38 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, error, isError, isLoading } = useQuery(['comments', post.id], () => fetchComments(post.id));
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong!</h3>
+        <p>{`${error}`}</p>
+      </>
+    );
+  }
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+
+      <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
+      {updateMutation.isError && <p style={{color: 'red'}}>Error updating the Post.</p>}
+      {updateMutation.isLoading && <p style={{color: 'purple'}}>Updating the Post.</p>}
+      {updateMutation.isSuccess && <p style={{color: 'green'}}>Post has (not) been updated.</p>}
+      
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && <p style={{color: 'red'}}>Error deleting the Post.</p>}
+      {deleteMutation.isLoading && <p style={{color: 'purple'}}>Deleting the Post.</p>}
+      {deleteMutation.isSuccess && <p style={{color: 'green'}}>Post has (not) been deleted.</p>}
+
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
