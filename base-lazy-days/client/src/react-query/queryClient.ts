142f3b0cf3aa65d3ff7acac9e833200d18a05f9e
Ivan Tabarino <tabarino@outlook.com>
2022-05-23 14:26:35 +0100

    feat: transform and refetch data

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -20,6 +20,11 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       onError: queryErrorHandler,
+      staleTime: 600000, // 10 minutes -> by default it is 0
+      cacheTime: 900000, // 15 minutes -> by default it is 5 minutes (doesn't make sense for staleTime to exceed cacheTime)
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
     },
   },
 });
